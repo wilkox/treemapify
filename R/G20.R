f50f283496e80b6a781f46aef9ee7620faa0ecd7
bernhard <bernhard.meindl@statistik.gv.at>
2015-07-22 11:33:24 +0200

    fix missing documentation for G20 dataset

---
+++ R/G20.R
@@ -0,0 +1,22 @@
+#' G20-Dataset
+#'
+#' A dataset containing some general statistics on 20 countries.
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item Region: Factor with 8 Levels ("Africa","Asia","Eurasia","Europe","Middle East","North America"
+#' "Oceania","South America")
+#'   \item Country: Factor with 20 levels with country names.
+#'   \item Trade.mil.USD: Value of trades in millions of US-dollars.
+#'   \item Nom.GDP.mil.USD: Nominal GDP in millions of US-dollars.
+#'   \item HDI: Human development index
+#'   \item Population: Poplulation size
+#'   \item Economic.classification: Factor with two levels ("Advanced" and "Developing") with information about the economic status.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name G20
+#' @usage data(G20)
+#' @format A data frame with 20 rows and 7 variables
+NULL
