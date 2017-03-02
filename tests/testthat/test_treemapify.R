5a29c5452de2d2498049bd8555eb08dec1151f7c
David Wilkins <david@wilkox.org>
2017-03-02 16:27:36 +1100

    Add test for ggplotify

--- tests/testthat/test_treemapify.R
+++ tests/testthat/test_treemapify.R
@@ -11,3 +11,17 @@ test_that("treemapify doesn't thrown an error", {
   )
   expect_error(treeMapCoordinates, NA)
 })
+
+context("ggplotify")
+
+test_that("ggplotify doesn't thrown an error", {
+  treeMapCoordinates <- treemapify(
+    G20,
+    area = "Nom.GDP.mil.USD",
+    fill = "HDI",
+    label = "Country",
+    group = "Region"
+  )
+  treemap <- ggplotify(treeMapCoordinates)
+  expect_error(treemap, NA)
+})
