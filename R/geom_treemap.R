3370c298953ea494ef4f88154b12a0a88eef81d6
David Wilkins <david@wilkox.org>
2017-03-04 18:23:41 +1100

    Write geom_treemap

---
+++ R/geom_treemap.R
@@ -0,0 +1,86 @@
+geom_treemap <- function(
+  mapping = NULL,
+  data = NULL,
+  stat = "identity",
+  position = "identity",
+  na.rm = FALSE,
+  show.legend = NA,
+  inherit.aes = TRUE,
+  ...
+) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomTreemap,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomTreemap <- ggproto(
+  "GeomTreemap",
+  Geom,
+  required_aes = c("area", "fill"),
+  default_aes = aes(
+    colour = "black",
+    fill = "grey35",
+    size = 0.5,
+    linetype = 1,
+    alpha = 1
+  ),
+  draw_key = draw_key_polygon,
+
+  draw_panel = function(data, panel_scales, coord) {
+
+    data <- coord$transform(data, panel_scales)
+    data$id <- 1:nrow(data)
+
+    # Generate treemap layout for data
+    params <- list(
+      data = data,
+      area = "area",
+      fill = "fill",
+      xlim = c(0, 1),
+      ylim = c(0, 1),
+      label = "id"
+    )
+    if (!all(data$group == -1)) {
+      params$group <- "group"
+    }
+    layout <- do.call(treemapify, params)
+
+    # Merge layout back into main data
+    names(layout)[names(layout) == "label"] <- "id"
+    layout <- layout[c("id", "xmin", "xmax", "ymin", "ymax")]
+    data <- merge(data, layout, by = "id")
+
+    # Draw rects
+    grob <- rectGrob(
+      x = data$xmin,
+      width = data$xmax - data$xmin,
+      y = data$ymax,
+      height = data$ymax - data$ymin,
+      default.units = "native",
+      just = c("left", "top"),
+      gp = gpar(
+        col = data$colour,
+        fill = alpha(data$fill, data$alpha),
+        lwd = data$size,
+        lty = data$linetype,
+        lineend = "butt"
+      )
+    )
+    grob$name <- grid::grobName(grob, "geom_treemap")
+    grob
+  }
+)
