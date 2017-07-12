52eb0a4d73a633ca17418c6b05c3df9f7e902f50
David Wilkins <david@wilkox.org>
2017-07-12 20:01:26 +0930

    Work on tests

--- test.R
+++ test.R
@@ -1,11 +1,20 @@
 library(devtools)
+library(tweenr)
+library(gganimate)
 load_all()
 
-ggplot(G20, aes(
+G20_alt <- G20
+G20_alt$GDP.mil.USD <- sample(G20$GDP.mil.USD, nrow(G20))
+G20_alt$HDI <- sample(G20$HDI, nrow(G20))
+
+tweened <- tween_states(list(G20, G20_alt), tweenlength = 20, statelength = 100, ease = 'cubic-in-out', nframes = 50)
+
+p <- ggplot(tweened, aes(
   area = GDP.mil.USD,
   fill = HDI,
   label = Country,
   subgroup = Region,
+  frame = .frame
   )) +
   geom_treemap(striped = T) +
   geom_treemap_subgroup_border(striped = T) +
@@ -24,3 +33,6 @@ ggplot(G20, aes(
     reflow = T,
     striped = T
   )
+  
+animation::ani.options(interval = 1/15)
+gganimate(p, "G20.gif", title_frame = F, ani.width = 400, ani.height = 400)
