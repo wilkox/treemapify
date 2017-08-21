a60aa860e2518e1eadb6d527ef8ab0df8f62aa17
David Wilkins <david@wilkox.org>
2017-08-21 12:09:13 +1000

    Deprecate ggplotify

--- tests/testthat/test_treemapify.R
+++ tests/testthat/test_treemapify.R
@@ -25,18 +25,3 @@ test_that("treemapify_fixed doesn't thrown an error or warning", {
   expect_error(treeMapCoordinates, NA)
   expect_warning(treeMapCoordinates, NA)
 })
-
-context("ggplotify")
-
-test_that("ggplotify doesn't thrown an error or warning", {
-  treeMapCoordinates <- treemapify(
-    G20,
-    area = "GDP.mil.USD",
-    fill = "HDI",
-    label = "Country",
-    group = "Region"
-  )
-  treemap <- ggplotify(treeMapCoordinates)
-  expect_error(treemap, NA)
-  expect_warning(treemap, NA)
-})
