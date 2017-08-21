b3aeecbf8c0bb0cfdd0cce84ed680fa2d90ff62a
David Wilkins <david@wilkox.org>
2017-08-21 14:07:40 +1000

    Document G20 dataset

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Statistics on the G-20 group of major world economies.
+#'
+#' A dataset containing economic and demographic statistics about members of the
+#' G-20 group of major world economies.
+#'
+#' @format A data frame with 20 rows and five variables:
+#' \describe{
+#'   \item{Region}{the country's region}
+#'   \item{Country}{the country}
+#'   \item{GDP.mil.USD}{the country's GDP, in millions of US dollars}
+#'   \item{HDI}{the country's Human Development Index}
+#'   \item{Econ.classification}{the country's economic classification}
+#' }
+#' @source \url{http://en.wikipedia.org/wiki/G-20_major_economies}
+"G20"
